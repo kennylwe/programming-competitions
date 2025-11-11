cc65d5d5120eb5f8b5610794f04a72820cdbd718
kenny <kenjlee09@gmail.com>
2025-11-10 18:57:44 -0800

    ride

--- USACO/usaco-practice/ride1.cpp
+++ USACO/usaco-practice/ride1.cpp
@@ -5,5 +5,19 @@ LANG: PYTHON3
 PROG: ride
 */
 
+#include <iostream>
+#include <string>
+#include <fstream>
 
+using namespace std;
+int main() {
+   string line;
+   ifstream ride ("ride.txt");
+   if (ride.is_open()) {
+      while (getline (ride, line) ) cout << line << "\n";
+   }
+   ride.close();
+
+
+}
 
