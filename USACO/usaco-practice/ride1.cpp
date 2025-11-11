e4deb63120b3012f178dd80a4e515151b048158a
kenny <kenjlee09@gmail.com>
2025-11-11 14:30:07 -0800

    waaaa

--- USACO/usaco-practice/ride1.cpp
+++ USACO/usaco-practice/ride1.cpp
@@ -1,22 +1,46 @@
 
 /*
 ID: kenjlee1
-LANG: PYTHON3
+LANG: C++
 PROG: ride
 */
 
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <list>
 
 using namespace std;
 int main() {
    string line;
+   list<string> list;
    ifstream ride ("ride.txt");
+   int sum1 = 1;
+   int sum2 = 1;
    if (ride.is_open()) {
-      while (getline (ride, line) ) cout << line << "\n";
+      while (getline (ride, line)) list.push_back(line);
    }
+
+
    ride.close();
+
+   for (char c : list.front()) {
+      sum1 *= c - 66;
+   }
+   for (char c : list.back()) {
+      sum2 *= c - 66;
+   }
+   std::ofstream myfile ("output.out");
+   
+   if (sum1 % 47 == sum2 % 47) {
+      myfile << "GO";
+   }
+   else {
+      myfile << "STAY";
+   }
+   myfile.close();
+
+
 
 
 }
