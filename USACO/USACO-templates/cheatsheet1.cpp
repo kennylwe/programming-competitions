07ac0f9785620cd6b3e01a761e2aea737447672b
kenny lee <kenjlee09@gmail.com>
2026-01-11 22:18:41 -0800

    new stuff wow

---
+++ USACO/USACO-templates/cheatsheet1.cpp
@@ -0,0 +1,76 @@
+// Headers {{{
+#include <algorithm>
+#include <cassert>
+#include <cstring>
+#include <iostream>
+#include <iterator>
+#include <map>
+#include <math.h>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <string>
+#include <tuple>
+#include <vector>
+
+using namespace std;
+
+void setIO(string name = "") {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    if (name.size()) {
+        freopen((name + ".in").c_str(), "r", stdin);
+        freopen((name + ".out").c_str(), "w", stdout);
+    }
+}
+
+//bad practice to use these outside of competitive programming
+
+// Types
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef string str;
+
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+
+// Vectors
+#define sz(x) int((x).size())
+#define all(x) begin(x), end(x)
+#define sortVec(x) sort(all(x))
+#define pb push_back
+
+// Loops
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define F0R(i, a) FOR(i, 0, a)
+#define ROF(i, a, b) for (int i = (b)-1; i >= (a); --i)
+#define R0F(i, a) ROF(i, 0, a)
+#define foreach(a, x) for (auto &a : x)
+
+// }}}
+
+// Problem Statement {{{
+
+// }}}
+
+void solve() {
+    int N;
+    cin >> N;
+}
+
+int T;
+
+int main() {
+    setIO("test");
+
+    cin >> T;
+    FOR(i, 1, T + 1) {
+        cout << "Case #" << i << ": ";
+        solve();
+    }
+
+    return 0;
+}
---
+++ USACO/usaco-practice/hello-world.cpp
@@ -0,0 +1,75 @@
+/*
+ID: kenjlee1
+LANG: C++
+PROG: ride
+*/
+
+
+#include <iostream>
+// #include <stdio>
+#include <algorithm>
+#include <cassert>
+#include <cstring>
+#include <iterator>
+#include <map>
+#include <math.h>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <string>
+#include <tuple>
+#include <vector>
+
+
+using namespace std;
+
+void setIO(string name = "") {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    if (name.size()) {
+        freopen((name + ".in").c_str(), "r", stdin);
+        freopen((name + ".out").c_str(), "w", stdout);
+    }
+}
+
+int calcscore(string group) {
+    int prod = 1;
+
+    for (char c : group) {
+        prod *= (c - 'A' + 1);
+    }
+
+    return prod;
+
+
+}
+
+int main() {
+    setIO("ride");
+    string group;
+    string comet;
+    cin >> group >> comet;
+
+    cerr << group << endl;
+    cerr << comet << endl;
+
+
+    if (calcscore(group) % 47 == calcscore(comet) % 47) {
+        cout << "GO" << endl;        
+    }
+    else {
+        cout << "STAY" << endl;
+    }
+    
+    
+    // int N;
+    // cin >> N;
+    // int y;
+    // cin >> y;
+    // cout << N + y << endl;
+
+
+
+    return 0;
+}
+    
