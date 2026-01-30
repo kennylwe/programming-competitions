4bca923d4360aa04d29dbdc3e6889a613782b385
kenny lee <kenjlee09@gmail.com>
2026-01-29 19:45:42 -0800

    usaco photoshoot

---
+++ USACO/Chip-Exchange/Chip-Exchange.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+// #include <stdio>
+#include <algorithm>
+#include <cassert>
+#include <cstring>
+#include <iterator>
+#include <map>
+#include <math.h>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <string>
+#include <tuple>
+#include <vector>
+
